2b8d7fade82488453cd57e50e3505a86decaa805
Maksim Goncharov <mgoncharov@jfusion.ru>
2022-06-01 13:28:10 +0300

    Refactoring

---
+++ src/app/modules/game/game.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GameComponent } from './components/game/game.component';
+
+@NgModule({
+  declarations: [
+    GameComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    GameComponent
+  ]
+})
+export class GameModule { }
---
+++ src/app/modules/game/models/item.ts
@@ -0,0 +1,13 @@
+export interface Item {
+  /**
+   * value % 2 === 0
+   */
+  value: number;
+
+  row: number;
+  col: number;
+  /**
+   * Must be deleted on next tick if true
+   */
+  isOnDelete?: boolean;
+}
